d6091fe203fd4e9d7182120b109fe057e2beda76
Piper McCorkle <contact@piperswe.me>
2026-09-03 19:27:44 +0200

    remove unused bazel components

--- projects/hello_world/hello_java/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.github.piperswe.codebase.projects.hello_world.hello_java;
-
-class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
--- projects/hello_world/hello_ts/hello.ts
+++
@@ -1,7 +0,0 @@
-import { Vector2 } from "$/lib/ts/vector/vector.js";
-
-console.log("Hello, world!");
-const a = new Vector2(1, 2);
-const b = new Vector2(2, 54);
-console.log(`${a.toString()} + ${b.toString()} = ${a.add(b).toString()}`);
-console.log(`${a.toString()} is ${a.angleDegrees(b)}deg to ${b.toString()}`);
